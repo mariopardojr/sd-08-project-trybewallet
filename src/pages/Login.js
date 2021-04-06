525b8fa2f393f3b8eed761ec57d0b928b359db6f
Mario Pardo <mariio.pardojr@gmail.com>
2021-04-06 18:14:53 -0300

    Adiciona estilo na tela de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { loginAction } from '../actions';
+import styles from './Login.module.css';
+import walletIcon from '../icons/walletIcon.svg';
 
 class Login extends React.Component {
   constructor() {
@@ -22,7 +24,7 @@ class Login extends React.Component {
   formValidation() {
     const { email, password } = this.state;
     const PASSWORD_LENGTH = 6;
-    const regexEmail = /^[\w]+@([\w]+\.)+[\w]{2,4}$/gi;
+    const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
     const emailIsValid = regexEmail.test(email);
     const passwordIsValid = password.length >= PASSWORD_LENGTH;
     const disabled = !(emailIsValid && passwordIsValid);
@@ -49,34 +51,45 @@ class Login extends React.Component {
   render() {
     const { email, disabled, redirect } = this.state;
     return (
-      <div>
-        <h2>Login</h2>
-        <form>
-          <input
-            type="text"
-            name="email"
-            value={ email }
-            placeholder="email"
-            data-testid="email-input"
-            onChange={ this.handleChange }
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="senha"
-            data-testid="password-input"
-            onChange={ this.handleChange }
-          />
-          <button
-            type="button"
-            disabled={ disabled }
-            onClick={ this.handleClick }
-          >
-            Entrar
-          </button>
-          { redirect ? <Redirect to="/carteira" /> : '' }
-        </form>
-      </div>
+      <main>
+        <div className={ styles.container }>
+          <div className={ styles.loginHeader }>
+            <h2>Trybe Wallet</h2>
+            <img className={ styles.icon } src={ walletIcon } alt="Wallet Icon" />
+          </div>
+          <form>
+            <div className={ styles.inputField }>
+              <input
+                type="text"
+                name="email"
+                value={ email }
+                placeholder="email"
+                data-testid="email-input"
+                onChange={ this.handleChange }
+              />
+            </div>
+            <div className={ styles.inputField }>
+              <input
+                type="password"
+                name="password"
+                placeholder="senha"
+                data-testid="password-input"
+                onChange={ this.handleChange }
+              />
+            </div>
+            <div className={ styles.inputField }>
+              <button
+                type="button"
+                disabled={ disabled }
+                onClick={ this.handleClick }
+              >
+                Entrar
+              </button>
+            </div>
+            { redirect ? <Redirect to="/carteira" /> : '' }
+          </form>
+        </div>
+      </main>
     );
   }
 }
